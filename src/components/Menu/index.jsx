768b5c4aced2050dddcd13a8bfaab203055ffcc0
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-18 18:17:57 -0500

    created menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <div className="Menu">
+      <h1 className="Menu_title">Menu</h1>
+      <div className="Menu_navbar">
+        <Link to="/" className="Menu_navbar_button">
+          Home
+        </Link>
+        <Link to="detalle/:id" className="Menu_navbar_button">
+          OneProduct
+        </Link>
+        <Link to="/About" className="Menu_navbar_button">
+          About
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Menu;
