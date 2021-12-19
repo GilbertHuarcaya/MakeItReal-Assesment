94c751d3a41bba6720ea046711d00fe22d997a31
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-18 21:21:21 -0500

    product page to show one product detail

---
+++ src/pages/Product/index.jsx
@@ -0,0 +1,11 @@
+import ProductDetail from '../../components/ProductDetail';
+
+const Product = () => {
+  return (
+    <div className="ProductDetail">
+      <ProductDetail />
+    </div>
+  );
+};
+
+export default Product;
