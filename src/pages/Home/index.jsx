9432bec1edf0f6f5c5f3f87feb62a8f5fe743bf0
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-18 21:22:15 -0500

    Home to show products from api

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Products from '../../components/Products';
+
+const Home = () => {
+  return (
+    <div className="Products">
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
