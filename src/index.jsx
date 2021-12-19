3c158dc9947b28f64903903481c20f012c97916c
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-18 21:20:37 -0500

    updated with new routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ProductProvider } from './product_context';
+
+import App from './app';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ProductProvider>
-      <App />
-    </ProductProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
