4493f5ffb60956a9e7135b6d77639b65182cde73
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-20 18:54:15 -0500

    header con estilos terminados

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Hedader_title">Fabulous Store</h1>
+    </div>
+  );
+};
+
+export default Header;
