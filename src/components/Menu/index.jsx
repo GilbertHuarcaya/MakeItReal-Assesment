ef377c30ce4cebaf5009ba01da3bcab139dc97ff
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-20 18:54:56 -0500

    Menu con funcionalidad de ocultar la url del producto si no se encuentra dentro de detalles

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,21 +1,61 @@
-import { Link, Outlet } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './styles.scss';
 
 const Menu = () => {
+  const [toggleClassBtnMenu, setToggleCLassBtnMenu] = useState('false');
+  const [productLink, setProductLnk] = useState();
+
+  const handlerMenu = () => {
+    if (!toggleClassBtnMenu) return setToggleCLassBtnMenu(true);
+    return setToggleCLassBtnMenu(false);
+  };
+  const urlactual = useLocation().pathname;
+  const pattern = /\/[0-9]+$/;
+
+  useEffect(() => {
+    setProductLnk(urlactual.match(pattern));
+  }, [useLocation()]);
+
   return (
     <div className="Menu">
-      <h1 className="Menu_title">Menu</h1>
-      <div className="Menu_navbar">
-        <Link to="/" className="Menu_navbar_button">
+      <nav
+        className={
+          toggleClassBtnMenu
+            ? 'Menu__navbar'
+            : 'Menu__navbar is-active-menu-navbar'
+        }
+      >
+        <Link to="/" className="Menu__navbar__a" onClick={handlerMenu}>
           Home
         </Link>
-        <Link to={`detalle/${1}`} className="Menu_navbar_button">
-          OneProduct
-        </Link>
-        <Link to="/About" className="Menu_navbar_button">
+        {!pattern.test(urlactual) ? (
+          <p className="Menu__navbar__a" disabled>
+            Product
+          </p>
+        ) : (
+          <Link
+            to={`/detalle${productLink}`}
+            className="Menu__navbar__a"
+            hidden={!pattern.test(urlactual)}
+            onClick={handlerMenu}
+          >
+            Product
+          </Link>
+        )}
+
+        <Link to="/About" className="Menu__navbar__a" onClick={handlerMenu}>
           About
         </Link>
+      </nav>
+      <div className="Menu__logo">
+        <button
+          className="Menu__logo__button"
+          type="button"
+          aria-label="Menu"
+          onClick={handlerMenu}
+        />
       </div>
-      <Outlet />
     </div>
   );
 };
