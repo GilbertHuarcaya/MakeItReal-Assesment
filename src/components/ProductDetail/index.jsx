bc9f65b94554a7d77d93db289d4e7f00c69239af
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-20 21:23:06 -0500

    updated details with spinner and css

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getProduct } from '../../services/products';
+import './styles.scss';
+import Spinner from '../../imgs/Spin.svg';
 
 const ProductDetail = () => {
   const { productId } = useParams();
@@ -17,16 +19,34 @@ const ProductDetail = () => {
   return (
     <main className="ProductDetail">
       {product ? (
-        <div>
-          <h2>Price: ${product.price}</h2>
-          <img src={product.image} alt={product.title} />
-          <p>
-            {product.title}: {product.id}
+        <div className="ProductDetail__card">
+          <h3 className="ProductDetail__card__subtitle">
+            Category: {product.category}
+          </h3>
+
+          <h2 className="ProductDetail__card__name">{product.title}</h2>
+
+          <div className="ProductDetail__card__photo">
+            <img
+              className="ProductDetail__card__img"
+              src={product.image}
+              alt={product.title}
+            />
+          </div>
+          <h2 className="ProductDetail__card__price">
+            Price: ${product.price}
+          </h2>
+          <h3 className="ProductDetail__card__subtitle">Description:</h3>
+          <p className="ProductDetail__card__description">
+            {product.description}
           </p>
-          <p>Description: {product.description}</p>
+          <h3 className="ProductDetail__card__subtitle">Rating:</h3>
+          <p className="ProductDetail__card__description">
+            {product.rating.rate} stars from {product.rating.count} votes
+          </p>
         </div>
       ) : (
-        <h1>Loading ...</h1>
+        <img src={Spinner} alt="Spinner" className="Products__loading" />
       )}
     </main>
   );
