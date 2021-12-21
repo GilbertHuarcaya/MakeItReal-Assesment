84881a1a3db83d2df54f0b4acf53e485178ccba5
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-20 21:22:48 -0500

    updated products with spinner and css

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,20 +1,25 @@
 import ProductCard from '../ProductCard';
 import { useStateProduct } from './Context';
+import './styles.scss';
+import Spinner from '../../imgs/Spin.svg';
 
 const Products = () => {
   const { products } = useStateProduct();
 
   return (
-    <div>
-      {products.length > 0 ? (
-        products.map((product) => (
-          <div key={product.id}>
-            <ProductCard product={product} />
-          </div>
-        ))
-      ) : (
-        <h1>Loading ...</h1>
-      )}
+    <div className="Products">
+      <h1 className="Products__title">Products</h1>
+      <div className="Products__cards">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <div key={product.id}>
+              <ProductCard product={product} />
+            </div>
+          ))
+        ) : (
+          <img src={Spinner} alt="Spinner" className="Products__loading" />
+        )}
+      </div>
     </div>
   );
 };
