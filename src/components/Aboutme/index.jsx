4348929b0b0dbfe557fd4dbd3006e9aa7fa74ddc
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-20 21:23:39 -0500

    updated aboutme with css responsive

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -1,14 +1,17 @@
 import './styles.scss';
+import Github from '../../imgs/github.svg';
 
 const Aboutme = () => {
   return (
     <div className="About">
       <div className="About__card">
-        <h1 className="About__title">Gilbert Alonso Huarcaya Matias</h1>
+        <h1 className="About__title">
+          Hola! me llamo Gilbert Alonso Huarcaya Matias
+        </h1>
         <div className="About__info">
           <p className="About__description">
             Soy un desarrollador FullStack vivo en Perú tengo 25 años y soy
-            estudiante de Make it Real
+            estudiante de Make it Real de tiempo completo
           </p>
           <img
             src="https://scontent.flim16-3.fna.fbcdn.net/v/t1.6435-9/54349910_2187685074626055_2012416216555061248_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=l4vYOQmuOHYAX9ChJGX&_nc_ht=scontent.flim16-3.fna&oh=00_AT8bI0HSsmMXnT9txBvKMd2Z_lEpIbSFHXYTZGdo_QHifg&oe=61E35D69"
@@ -20,13 +23,18 @@ const Aboutme = () => {
           Temas aprendidos en el programa de Make it Real
         </h3>
         <ul className="About__list">
-          <li>Scrum</li>
-          <li>React</li>
-          <li>Context</li>
+          <li>SCRUM</li>
+          <li>REACT</li>
+          <li>CONTEXT</li>
         </ul>
-        <p className="About__email">gilberthuarcaya@gmail.com</p>
-        <a className="About__a" href="https://github.com/GilbertHuarcaya">
-          Github
+        <p className="About__email">Mi correo es: gilberthuarcaya@gmail.com</p>
+        <a
+          className="About__a"
+          href="https://github.com/GilbertHuarcaya"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="About__a__img" src={Github} alt="githublink" />
         </a>
       </div>
     </div>
