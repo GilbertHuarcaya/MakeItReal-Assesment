0a0b026e134a83bcac3377c08037f894a1f22484
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-12-28 19:37:10 -0500

    useEffect coutdown to 0 even if page changes

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Menu from '../Menu';
 import Header from '../Header';
-/* import './styles.scss'; */
 
 const Layout = () => {
+  useEffect(() => {
+    const id = setInterval(() => {
+      Object.entries(localStorage)
+        .filter(([key, value]) => Number(value) > 0)
+        .forEach(([key, value]) => {
+          localStorage.setItem(key, Number(value) - 1);
+        });
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
   return (
     <>
       <Header />
